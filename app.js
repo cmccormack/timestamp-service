b80dada7d04af22a349ebc31f2e911d1a4ed4a05
cmccormack <mccormack.christopher@gmail.com>
2017-10-23 20:28:51 -0700

    Change console output

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
 
+app.get('/', (req, res) => {
+  res.send('Welcome!')
+})
 
+app.get('/:date', (req, res) => {
+  res.send(req.params.date)
+})
 
 
 const server = app.listen(port, ()=>{
   const { port, address } = server.address()
-  console.log(`Node server started on ${address}:${port}.`)
+  console.log(`Server started on ${address}:${port}.`)
 })
