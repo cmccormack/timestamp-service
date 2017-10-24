64d7e99c448e272ac76ba5c75ce565ddf35da25c
cmccormack <mccormack.christopher@gmail.com>
2017-10-23 21:57:58 -0700

    User stories are all tested and successful

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express')
+const engines = require('consolidate')
+const path = require('path')
+
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000
+const months = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+]
+
+app.engine('html', engines.nunjucks)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'html')
 
 app.get('/', (req, res) => {
-  res.send('Welcome!')
+  res.render('examples', {title:'Timestamp Microservice'})
 })
 
 app.get('/:date', (req, res) => {
-  res.send(req.params.date)
+  let output = {unix: null, natural: null}
+
+  let dateparam = req.params.date
+  if (Number(dateparam)) {
+    dateparam = Number(dateparam) * 1000
+  }
+  const date = new Date(dateparam)
+  if (date.toDateString() !== "Invalid Date") {
+    const year = date.getFullYear()
+    const month = months[date.getMonth()]
+    const day = date.getUTCDate()
+    output.unix = date.getTime() / 1000
+    output.natural = `${month} ${day}, ${year}`
+  }
+  res.type('json')
+  res.send(JSON.stringify(output))
+  
 })
 
 
